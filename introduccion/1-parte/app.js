5800f547dd4242e50e1069641384bbc2eac9d419
Javier Balsas <javier9518@gmail.com>
2020-05-27 22:28:10 -0500

    typescript argumentos opcionales

--- introduccion/1-parte/app.js
+++ introduccion/1-parte/app.js
@@ -1,25 +1,6 @@
 "use strict";
-(function () {
-    //tipo de variables
-    var mensaje = "hola";
-    var numero = 123; //number
-    var booleano = true; //true or false
-    var hoy = new Date(); //fecha
-    var variableRandom; //aceptan tipos
-    var tipoObject = {
-        nombre: "juan",
-        edad: 23,
-    };
-    tipoObject = {
-        nombre: "mia khalifa",
-        edad: 27,
-    };
-    function getEdad(numero) {
-        return numero * 400;
-    }
-    // @TODO: 'literals'
-    var nombre = "Gato";
-    var apellido = "Con botas";
-    var edad = 34;
-    var salida = nombre + " " + apellido + " (" + getEdad(edad) + ")";
-});
+var activar = function (quien, momento, objeto) {
+    if (objeto === void 0) { objeto = "tortugas"; }
+    return quien + " me llamo " + objeto + " " + (momento == null ? "" : momento);
+};
+console.log(activar("Juan", "batidora"));
