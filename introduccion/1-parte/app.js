c10284d81794332926775577297f935c718590f0
Javier Balsas <javier9518@gmail.com>
2020-05-27 21:05:44 -0500

    typescript variables

---
+++ introduccion/1-parte/app.js
@@ -0,0 +1,11 @@
+"use strict";
+(function () {
+    //variables
+    var mensaje = "ey q xopa";
+    var test = "no cambia";
+    console.log(mensaje);
+    if (true) {
+        var mensaje_1 = "mundo";
+    }
+    console.log(mensaje);
+});
---
+++ introduccion/1-parte/app.ts
@@ -0,0 +1,12 @@
+() => {
+  //variables
+  let mensaje = "ey q xopa";
+  const test = "no cambia";
+  console.log(mensaje);
+
+  if (true) {
+    let mensaje = "mundo";
+  }
+
+  console.log(mensaje);
+};
