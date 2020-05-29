31686bf3532fafcb3366a0f71c72fde698b3dbd8
Javier Balsas <javier9518@gmail.com>
2020-05-29 13:27:33 -0500

    desestructuracion de arreglos y objetos

--- introduccion/1-parte/app.js
+++ introduccion/1-parte/app.js
@@ -1,17 +1,29 @@
 "use strict";
 (function () {
-    var miFunction = function (a) { return a; };
-    var sumarN = function (a, b) { return a + b; };
-    console.log(miFunction("hola"));
-    console.log(sumarN(5, 8));
-    var hul = {
-        nombre: "Hulk",
-        smash: function () {
-            var _this = this;
-            setTimeout(function () {
-                console.log(_this.nombre + " demencia.");
-            }, 1000);
-        },
+    // destructuracion de objetos
+    var avenger = {
+        nombre: "Bad bunny",
+        clave: "Cantante",
+        poder: "ninguno",
     };
-    hul.smash();
+    var nombre = avenger.nombre, clave = avenger.clave;
+    console.log(nombre);
+    console.log(clave);
+    var extraer = function (_a) {
+        var poder = _a.poder, nombre = _a.nombre;
+        console.log(nombre);
+        console.log(poder);
+    };
+    extraer(avenger);
+    var avengers = ["thor", "Ironman", "Spiderman"];
+    // const [loki, hombre, chino] = avengers;
+    var loki = avengers[2];
+    console.log(loki);
+    // console.log(hombre);
+    // console.log(chino);
+    var extraerArr = function (_a) {
+        var chicharito = _a[0], ironman = _a[1], spiderman = _a[2];
+        console.log(chicharito), console.log(ironman), console.log(spiderman);
+    };
+    extraerArr(avengers);
 })();
