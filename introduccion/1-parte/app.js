edecbd0fd169c006ed751cbdef2f9b9a8ac1dcce
Javier Balsas <javier9518@gmail.com>
2020-05-29 18:08:42 -0500

    Clases

--- introduccion/1-parte/app.js
+++ introduccion/1-parte/app.js
@@ -2,10 +2,17 @@
 (() => {
     //clases
     class TestClass {
-        constructor(nombre, equipo, nombreReal) {
+        // nombre: string;
+        // equipo: string;
+        // nombreReal: string;
+        // puedePelear?: boolean;
+        // peleaGanadas?: number;
+        constructor(nombre, equipo, nombreReal, puedePelear = true, peleaGanadas = 0) {
             this.nombre = nombre;
             this.equipo = equipo;
             this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleaGanadas = peleaGanadas;
         }
     }
     const hulk = new TestClass("cacaroto", "brokoli", "Garen de demacia");
