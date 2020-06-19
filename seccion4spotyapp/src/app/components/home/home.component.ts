60cee47d7ccf9a9838a82a740164d1b86e903cd7
Javier Balsas <javier9518@gmail.com>
2020-06-18 22:52:38 -0500

    spotty error msj

--- seccion4spotyapp/src/app/components/home/home.component.ts
+++ seccion4spotyapp/src/app/components/home/home.component.ts
@@ -10,15 +10,25 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 export class HomeComponent implements OnInit {
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: string;
   constructor(
     private http: HttpClient,
     private spotyfyservice: SpotifyService
   ) {
+    this.error = false;
     this.loading = true;
-    this.spotyfyservice.getNewsReleases().subscribe((data: any) => {
-      this.nuevasCanciones = data;
-      this.loading = false;
-    });
+    this.spotyfyservice.getNewsReleases().subscribe(
+      (data: any) => {
+        this.nuevasCanciones = data;
+        this.loading = false;
+      },
+      (err) => {
+        this.loading = false;
+        this.error = true;
+        this.mensajeError = err.error.error.message;
+      }
+    );
   }
 
   ngOnInit(): void {}
