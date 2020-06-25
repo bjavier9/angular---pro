219df0532fe52845a8a559243b04010b322ad368
Javier Balsas <javier9518@gmail.com>
2020-06-24 22:53:25 -0500

    ionic guardar listas

--- deseos/src/app/pages/agregar/agregar.page.ts
+++ deseos/src/app/pages/agregar/agregar.page.ts
@@ -28,4 +28,20 @@ export class AgregarPage implements OnInit {
 		this.nombreItem = '';
 		this.deseosServices.guardarStorage();
 	}
+	cambioCheck(item: ListaItem) {
+		const pendientes = this.lista.items.filter((itemData) => !itemData.completado).length;
+		if (pendientes === 0) {
+			console.log(pendientes);
+			this.lista.terminadoEn = new Date();
+			this.lista.terminada = true;
+		} else {
+			this.lista.terminadoEn = null;
+			this.lista.terminada = false;
+		}
+		this.deseosServices.guardarStorage();
+	}
+	borrar(i: number) {
+		this.lista.items.splice(i, 1);
+		this.deseosServices.guardarStorage();
+	}
 }
